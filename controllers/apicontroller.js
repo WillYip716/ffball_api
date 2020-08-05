ee5efcb49308ec07489d6f0f63823ab8d9cf06ee
WillYip716 <william.yip.716@gmail.com>
2020-08-04 19:40:09 -0700

    update to save to league controller and installed login packets

--- controllers/apicontroller.js
+++ controllers/apicontroller.js
@@ -12,21 +12,13 @@ exports.player_info = function(req,res,next){
 
 exports.store_league = function(req,res,next){
     let playerlist = JSON.parse(JSON.stringify(req.body));
-    for(var element in playerlist){
-        player = new Player({
-            name:playerlist[element].name,
-            position: playerlist[element].position,
-            //owner:playerlist[element].owner,
-            rank:playerlist[element].rank,
-            auctionprice:playerlist[element].auctionprice,
-            keeperyears: playerlist[element].keeperyears
-        }).save(function(err, savedplayer) {
-            if (err) {
-                return next(err);
-            }
-            res.status(200);
-            return;
-        });
-    }
+
+    Player.insertMany(playerlist, (err, docs) => {
+        if(err){
+            return next(err);
+        }
+        res.status(200).json(docs);
+        console.log("finished upload");
+    });
     
 };
